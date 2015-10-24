4a8af61843cf05b6c3abb897b879cdb9e3a1e25f
starkwang <381152119@qq.com>
2015-10-24 15:30:31 +0800

    module

--- index.js
+++ index.js
@@ -1,54 +1,5 @@
 fs = require('fs');
+tokenizer = require('./tokenizer');
 fs.readFile('test.scss', 'utf-8', function(err, content) {
-    var result = [];
-    var symbol = ['{', '}', ':', ';', '$', ',', '(', ')', '.', '#', '~', '>', '*', '+', '[', ']', '=', '|', '^'];
-    var isInString = false;
-    var tmpString = '';
-    var add = function(target, word) {
-        if (this.length != 0) {
-            result.push(target);
-            target = '';
-        }
-        target += word;
-    }
-    for (var i = 0; i < content.length; i++) {
-
-        var t = content[i];
-        if (t == '\'' || t == '\"') {
-            if (isInString) {
-                tmpString += t;
-                isInString = false;
-                result.push(tmpString);
-                tmpString = '';
-            } else {
-                tmpString += t;
-                isInString = true;
-            }
-            continue;
-        }
-        if (isInString) {
-            //字符串状态
-            tmpString += t;
-        } else {
-            //非字符串状态
-            if (t == '\n' || t == ' ' || t == '    ') {
-                if (tmpString.length != 0) {
-                    result.push(tmpString);
-                    tmpString = '';
-                }
-                continue;
-            }
-            if (symbol.indexOf(t) != -1) {
-                if (tmpString.length != 0) {
-                    result.push(tmpString);
-                    tmpString = '';
-                }
-                result.push(t);
-                continue;
-            }
-            tmpString += t;
-        }
-
-    }
-    console.log(result);
+    tokenizer(content);
 })
